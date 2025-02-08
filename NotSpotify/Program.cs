1f0e2b3d649a68ac9b9ae820969019200beb801a
Michael Skotar <michaelskotar@gmail.com>
2025-02-08 18:49:10 -0500

    Add project files.

---
+++ NotSpotify/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace NotSpotify
+{
+    class Program
+    {
+        string youtubePlaylistURL = "";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YouTube Data API: Playlist Updates");
+            Console.WriteLine("==================================");
+        }
+    }
+}
