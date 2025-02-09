ec7043e360985ac9ec4c0d911e79d96e9755f7f0
Michael Skotar <michaelskotar@gmail.com>
2025-02-09 17:08:40 -0500

    Added the saveMP3 function

--- NotSpotify/Program.cs
+++ NotSpotify/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-
+using System.IO;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Upload;
 using Google.Apis.Util.Store;
 using Google.Apis.YouTube.v3;
 using Google.Apis.YouTube.v3.Data;
+using MediaToolkit;
+using MediaToolkit.Model;
+using VideoLibrary;
 
 namespace NotSpotify
 {
@@ -15,8 +18,35 @@ namespace NotSpotify
 
         static void Main(string[] args)
         {
-            Console.WriteLine("YouTube Data API: Playlist Updates");
+            Console.WriteLine("YouTube API Playlist Download");
             Console.WriteLine("==================================");
+
+            // Gets the youtube playlist
+
+        }
+
+        /// <summary>
+        /// Saves a video url to an mp3 in a specific folder
+        /// </summary>
+        /// <param name="SaveToFolder"></param>
+        /// <param name="VideoURL"></param>
+        /// <param name="MP3Name"></param>
+        private void SaveMP3(string SaveToFolder, string VideoURL, string MP3Name)
+        {
+            var source = @SaveToFolder;
+            var youtube = YouTube.Default;
+            var vid = youtube.GetVideo(VideoURL);
+            File.WriteAllBytes(source + vid.FullName, vid.GetBytes());
+
+            var inputFile = new MediaFile { Filename = source + vid.FullName };
+            var outputFile = new MediaFile { Filename = $"{MP3Name}.mp3" };
+
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+                engine.Convert(inputFile, outputFile);
+            }
         }
     }
 }
