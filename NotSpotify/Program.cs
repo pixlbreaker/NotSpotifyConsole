647a8de88e60203e48122f2865b547888d4f95b9
Michael Skotar <michaelskotar@gmail.com>
2025-02-10 17:07:01 -0500

    Changed it to be a static class

--- NotSpotify/Program.cs
+++ NotSpotify/Program.cs
@@ -12,16 +12,18 @@ using VideoLibrary;
 
 namespace NotSpotify
 {
-    class Program
+    public class Program
     {
         string youtubePlaylistURL = "";
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("YouTube API Playlist Download");
             Console.WriteLine("==================================");
 
             // Gets the youtube playlist
+            // Try and download a file
+            SaveMP3("", "https://www.youtube.com/watch?v=T6eK-2OQtew", "notlikeus.mp3");
 
         }
 
@@ -31,7 +33,7 @@ namespace NotSpotify
         /// <param name="SaveToFolder"></param>
         /// <param name="VideoURL"></param>
         /// <param name="MP3Name"></param>
-        private void SaveMP3(string SaveToFolder, string VideoURL, string MP3Name)
+        private static void SaveMP3(string SaveToFolder, string VideoURL, string MP3Name)
         {
             var source = @SaveToFolder;
             var youtube = YouTube.Default;
