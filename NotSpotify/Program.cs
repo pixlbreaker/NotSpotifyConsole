3733f47b3bf49b1f2c884378d9b364ff12037f8b
Michael Skotar <michaelskotar@gmail.com>
2025-02-18 17:04:05 -0500

    Still not working

--- NotSpotify/Program.cs
+++ NotSpotify/Program.cs
@@ -1,8 +1,11 @@
-﻿using MediaToolkit.Model;
-using MediaToolkit;
+﻿using MediaToolkit;
+using MediaToolkit.Model;
 using System;
 using System.IO;
 using VideoLibrary;
+using YoutubeExplode;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Videos.Streams;
 
 namespace NotSpotify
 {
@@ -15,25 +18,29 @@ namespace NotSpotify
             Console.WriteLine("YouTube API Playlist Download");
             Console.WriteLine("==================================");
 
+            DownloadVideo("this");
 
-            //bool downloaded = DownloadVideo("T6eK-2OQtew");
-            string path = Directory.GetCurrentDirectory() + "//Videos//";
-            SaveMP3(path, "https://www.youtube.com/watch?v=AF2MqFnPotc", "bbl");
+            //string path = Directory.GetCurrentDirectory() + "\\Videos\\";
+            //SaveMP3(path, "https://www.youtube.com/watch?v=AF2MqFnPotc", "bbl");
 
         }
 
-        static bool DownloadVideo(string id)
+        static async void DownloadVideo(string id)
         {
-            try
-            {
-                var VedioUrl = "https://www.youtube.com/embed/" + id + ".mp4";
-                var youTube = YouTube.Default;
-                var video = youTube.GetVideo(VedioUrl);
-                string path = Directory.GetCurrentDirectory() + "//Videos//" + id + ".mp4";
-                File.WriteAllBytes(path, video.GetBytes());
-            }
-            catch (Exception ex) { return false; }
-            return true;
+            var youtube = new YoutubeClient();
+            var videoUrl = "https://www.youtube.com/watch?v=AF2MqFnPotc";
+
+            //string path = Directory.GetCurrentDirectory() + "\\Videos\\" + "video.mp4";
+            //await youtube.Videos.DownloadAsync(videoUrl, "video.mp4");
+
+            var streamManifest = await youtube.Videos.
+            var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+            // Get the actual stream
+            var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+
+            // Download the stream to a file
+            await youtube.Videos.Streams.DownloadAsync(streamInfo, $"video.{streamInfo.Container}");
         }
 
         private static void SaveMP3(string SaveToFolder, string VideoURL, string MP3Name)
@@ -46,12 +53,9 @@ namespace NotSpotify
             var inputFile = new MediaFile { Filename = source + vid.FullName };
             var outputFile = new MediaFile { Filename = $"{MP3Name}.mp3" };
 
-            using (var engine = new Engine())
-            {
-                engine.GetMetadata(inputFile);
-
-                engine.Convert(inputFile, outputFile);
-            }
+            Engine engine = new Engine();
+            engine.GetMetadata(inputFile);
+            engine.Convert(inputFile, outputFile);
         }
     }
 }
